cb6e754987171a00a2fda49d29f221801c3218b3
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-10-02 11:07:51 -0500

    flickr added

---
+++ pythonweb.py
@@ -0,0 +1,27 @@
+from flickrapi import FlickrAPI
+
+from flask import Flask, render_template, request
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    searchText = None
+    flickrImage = None
+    flickrImage2 = None
+    if request.method == 'POST' and 'searchText' in request.form:
+        searchText = request.form['searchText']
+        FLICKR_PUBLIC =  os.environ['FLICKR_PUBLIC']
+        FLICKR_SECRET =  os.environ['FLICKR_SECRET']
+        flickr = FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format='parsed-json')
+        extras = 'url_c,url_l,url_o'
+        results = flickr.photos.search(text=searchText, per_page=5, extras=extras)
+        fetchPhotoURL = results['photos']['photo'][0]['url_c']
+        fetchPhotoURL2 = results['photos']['photo'][1]['url_c']
+
+    return render_template('index.html', flickrImage=fetchPhotoURL, flickrImage2=fetchPhotoURL2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
